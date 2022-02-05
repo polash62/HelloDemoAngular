f8d0d15f32112f26e76ab8cfe50bb1dea1878366
polash620 <polash.reply@gmail.com>
2022-02-05 12:46:18 +0600

    first commit for tree view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { TreeComponent } from './tree/tree.component';
+import { TreeViewComponent } from '@syncfusion/ej2-angular-navigations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeComponent,
+    TreeViewComponent
   ],
   imports: [
     BrowserModule
